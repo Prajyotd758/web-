78b50f5df719bb46a05d979463f69c1949881c8d
Prajyotd758 <107939295+Prajyotd758@users.noreply.github.com>
2023-05-31 22:39:32 +0530

    Add files via upload

---
+++ nfl/index.js
@@ -0,0 +1,25 @@
+// let str = "welcome to our webpage , we hope you like it , i'm jarvis im your guide. i will help you to reach your desired results "
+// let welcome = new SpeechSynthesisUtterance(str)
+// speechSynthesis.speak(welcome);
+// let con = document.querySelector('.container');
+
+// function changeop() {
+//     con.classList.add('add');
+// }
+
+// function speak() {
+//     speechSynthesis.cancel(welcome)
+//     let str = "enter username";
+//     let talk = new SpeechSynthesisUtterance(str);
+//     speechSynthesis.speak(talk);
+// }
+// function speak1() {
+//     let str = "enter password";
+//     let talk = new SpeechSynthesisUtterance(str);
+//     speechSynthesis.speak(talk);
+// }
+// function speak2() {
+//     let str = "login to proceed";
+//     let talk = new SpeechSynthesisUtterance(str);
+//     speechSynthesis.speak(talk);
+// }
